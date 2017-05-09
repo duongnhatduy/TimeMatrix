1ce1e18d84d29e60b972a8de01a524b5fb02531a
Duy Duong <duongnhhatduy@gmail.com>
2017-05-09 20:25:07 +0300

    separate styles

--- index.ios.js
+++ index.ios.js
@@ -11,6 +11,7 @@ import {
   Text,
   View
 } from 'react-native';
+import styles from "./styles.js";
 
 export default class TimeMatrix extends Component {
   render() {
@@ -30,24 +31,5 @@ export default class TimeMatrix extends Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('TimeMatrix', () => TimeMatrix);
